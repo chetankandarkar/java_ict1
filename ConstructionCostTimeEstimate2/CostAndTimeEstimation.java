346098c636141b40170f1aad631ebf710b65d600
Chetan Kandarkar <74239460+chetankandarkar@users.noreply.github.com>
2022-06-16 20:51:27 +0530

    Add files via upload

---
+++ ConstructionCostTimeEstimate2/CostAndTimeEstimation.java
@@ -0,0 +1,22 @@
+package com.cts.conctes.client;
+import com.cts.conctes.exception.ConstructionEstimationException;
+import com.cts.conctes.service.ConstructionProjectEstimationService;
+
+public class CostAndTimeEstimation {
+
+	public static void main(String[] args) throws ConstructionEstimationException
+	{
+		
+		ConstructionProjectEstimationService cpeService = new ConstructionProjectEstimationService();
+		//WRITE YOUR CODE HERE
+	
+		boolean isTrue=cpeService.addConstructionProjectDetails("inputfeed.txt");
+		if(isTrue) {
+		System.out.println("All are added successfully into the database");
+		}
+		else
+		{
+			System.out.println("database connection failed...");
+		}
+	}
+}
